841ffef114573f98eb627bc62fbc1851fe031429
Austin-Boedi <114024627+Austin-Boedi@users.noreply.github.com>
2025-03-31 14:39:49 -0500

    Responsive tweaks, style compat

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDB3IfJTVPOlvqyC12FHapudZxXx2Zqdi8",
-  authDomain: "arc---management.firebaseapp.com",
-  projectId: "arc---management",
-  storageBucket: "arc---management.firebasestorage.app",
-  messagingSenderId: "781159835872",
-  appId: "1:781159835872:web:809187b22badb011acc885",
-  measurementId: "G-5F14W1HLFK"
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDB3IfJTVPOlvqyC12FHapudZxXx2Zqdi8",
+    authDomain: "arc---management.firebaseapp.com",
+    projectId: "arc---management",
+    storageBucket: "arc---management.firebasestorage.app",
+    messagingSenderId: "781159835872",
+    appId: "1:781159835872:web:809187b22badb011acc885",
+    measurementId: "G-5F14W1HLFK"
+  }
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Optional: Firebase initialization (used only if needed in this context)
+
+const app = initializeApp(environment.firebase);
 const analytics = getAnalytics(app);
